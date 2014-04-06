3a076597a34bef8fe9f67e0326071fbe1dafa030
Michal Krupa <michalkrupa@gmail.com>
2014-04-05 21:07:00 -0500

    typo

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  before_filter :check_user
+
+  def profile
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def dashboard
+    respond_to do |format|
+      format.html
+    end
+  end
+
+private
+
+  def check_user
+    if !current_user
+      redirect_to landing_path, :flash => { :error => 'Please sign in to view this page' }
+    else
+      return
+    end
+  end
+
+end
