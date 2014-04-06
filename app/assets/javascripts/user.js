30ed3e808d2c822b352a56ebd84487c315381670
bjryan2 <bjryan2@illinois.edu>
2014-04-06 00:10:29 -0500

    fuck turbolinks

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,13 +1,19 @@
-$(document).ready(function(){
 
-  $('#tokenfield')
-  .tokenfield({
 
-    allowDuplicates: false,
-    limit: 10,
-    minLength: 2,
-    maxLength: 15,
+var ready;
+ready = function() {
+ $('#tokenfield')
+ .tokenfield({
 
-  });
+  allowDuplicates: false,
+  limit: 10,
+  minLength: 2,
+  maxLength: 15,
 
 });
+
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
