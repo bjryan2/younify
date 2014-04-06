7ff24661a3dd38cd8748494a8c248887b555b379
bjryan2 <bjryan2@illinois.edu>
2014-04-06 00:07:16 -0500

    fixed broken tags box

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,15 +1,6 @@
 $(document).ready(function(){
 
   $('#tokenfield')
-  .on('tokenfield:preparetoken', function (e) {
-    //cycle through classes for
-    var classes = ['red', 'blue', 'teal', 'green'];
-
-    var str = classes[Math.floor(Math.random()*classes.length)];
-    console.log(e.relatedTarget);
-    console.log(e);
-    $('#tokenfield').children('.token').last().addClass(str);
-  })
   .tokenfield({
 
     allowDuplicates: false,
