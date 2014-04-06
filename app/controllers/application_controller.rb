7b7f5cfb131ae6e2a8a84a704500fb53f6ac79e7
Michal Krupa <michalkrupa@gmail.com>
2014-04-06 02:48:16 -0500

    additional application filters, resume uploader and file format validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_filter :update_sanitized_params, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:email, :password, :password_confirmation, :biography, :resume)
+    end
+  end
+
+  def authenticate_user!
+    if current_user
+      return
+    else
+      redirect_to landing_index_path, :flash => { :error => 'Please sign in to view this page' }
+    end
+  end
+
 end
