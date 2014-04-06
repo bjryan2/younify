71f03400384e124e6b1ba8d0d403d1f992ff3478
Michal Krupa <michalkrupa@gmail.com>
2014-04-05 21:03:32 -0500

    user routes

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20140405215707) do
     t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
